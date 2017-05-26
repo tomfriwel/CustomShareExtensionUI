4b4530b449632e68ca04dd4d5ef930abc33a870e
tomfriwel <976932447@qq.com>
2017-05-26 10:18:06 +0800

    share extension show/dismiss animation

--- TomShareExtension/ShareExtensionViewController.m
+++ TomShareExtension/ShareExtensionViewController.m
@@ -23,13 +23,13 @@
     [super viewWillAppear:animated];
     
     self.view.transform = CGAffineTransformMakeTranslation(0, self.view.frame.size.height);
-    [UIView animateWithDuration:0.25 animations:^{
+    [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:0.0 options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionCurveLinear  animations:^{
         self.view.transform = CGAffineTransformIdentity;
-    }];
+    } completion:nil];
 }
 
 - (IBAction)dismissAction:(id)sender {
-    [UIView animateWithDuration:0.20 animations:^{
+    [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:0.0 options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionCurveLinear  animations:^{
         self.view.transform = CGAffineTransformMakeTranslation(0, self.view.frame.size.height);
     } completion:^(BOOL finished) {
         [self.extensionContext completeRequestReturningItems:nil completionHandler:nil];
